990434345970f6b2098ab11f0e310400731c73e0
Enrico Scherlies <e.scherlies@pm.me>
2024-10-22 23:38:56 +0200

    Add neovim

--- home.nix
+++ home.nix
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05";
 
   home.packages = [
     # Mac tools
@@ -74,4 +74,15 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Enable Neovim
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    viAlias = true;
+    extraConfig = ''
+      set number
+      set relativenumber
+    '';
+  };
 }
